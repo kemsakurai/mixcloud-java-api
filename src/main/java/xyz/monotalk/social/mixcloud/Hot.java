ff342e8667828e557fa637988c14547b572efa2f
Ken Sakurai <monotalk.xyz@gmail.com>
2022-10-31 02:01:07 +0900

    Update Hot.java

--- src/main/java/xyz/monotalk/social/mixcloud/Hot.java
+++ src/main/java/xyz/monotalk/social/mixcloud/Hot.java
@@ -16,9 +16,7 @@ import xyz.monotalk.social.mixcloud.internal.JsonUtils;
  */
 public class Hot implements Pathable<MixCloudResult<CloudCast>>, RequesterBuilder {
 
-    private String unused = null;
     private String unused1 = null;
-    private String unused2 = null;
     private Pathable pathable = null;
 
     /**
